4bb481fedbed05e07ac3430c30733c506875d73c
mohamed.morse <mohamed.morse@expertapps.com.sa>
2023-11-26 11:32:59 +0200

    setup top action bar 🙋‍♂️

--- app/src/main/java/com/morse/currency/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/morse/currency/ui/screens/home/HomeScreen.kt
@@ -1,2 +1,19 @@
 package com.morse.currency.ui.screens.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showSystemUi = true , showBackground = true)
+@Composable
+fun HomeScreen (){
+    Column {
+        HomeActionBar()
+    }
+}
+
+@Composable
+fun HomeActionBar (modifier : Modifier = Modifier){
+
+}
